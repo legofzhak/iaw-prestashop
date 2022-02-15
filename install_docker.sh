8d6a16f9b0fd4a7f4ad5e924255523e0501a0afb
Zakariya El Khattabi <78587203+legofzhak@users.noreply.github.com>
2022-02-15 03:49:06 +0100

    insat

---
+++ install_docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -x
+
+# Variables
+USERNAME=ubuntu
+
+# Actualizamos el sistema
+apt update
+
+# Descargamos el script
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+# Ejecutamos el script
+sudo sh get-docker.sh
+
+# Añadimos nuestro usuario al grupo docker
+usermod -aG docker $USERNAME
+
+# Actualizamos el grupo docker se hace aparte
+#newgrp docker
+
+# Iniciamos el servicio docker
+systemctl start docker
+
+# Configuramos para que el servicio se inicie automaticamente
+systemctl enable docker
